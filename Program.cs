652780516c79914815d6c06f19fdc52438b9a711
Mark Wu <wfwf1997@gmail.com>
2020-02-16 15:16:12 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimecodeUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
